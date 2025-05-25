72b7cb3ed5247f50ba9f47a49f8109aae1d4d46f
alvarofgomes <alvarogomes098gmail.com>
2025-05-24 21:16:05 -0300

    Colocando o Service

--- src/main/java/com/tarefas/Controle_de_Tarefas/model/Tarefa.java
+++ src/main/java/com/tarefas/Controle_de_Tarefas/model/Tarefa.java
@@ -20,8 +20,70 @@ public class Tarefa {
     private LocalDate dataEntrega;
 
     private boolean concluida;
+    
+    
 
-    @ManyToOne
+    public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+
+
+	public boolean isConcluida() {
+		return concluida;
+	}
+
+
+
+	public void setConcluida(boolean concluida) {
+		this.concluida = concluida;
+	}
+
+
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+
+
+	@ManyToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 }
---
+++ src/main/java/com/tarefas/Controle_de_Tarefas/service/UsuarioService.java
@@ -0,0 +1,28 @@
+package com.tarefas.Controle_de_Tarefas.service;
+
+import com.tarefas.Controle_de_Tarefas.model.Usuario;
+import com.tarefas.Controle_de_Tarefas.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario salvar(Usuario usuario) {
+        //não deixar cadastrar dois usuários com o mesmo email
+        Optional<Usuario> existente = usuarioRepository.findByEmail(usuario.getEmail());
+        if (existente.isPresent()) {
+            throw new RuntimeException("Já existe um usuário com esse email.");
+        }
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+}
